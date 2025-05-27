019cc58db537758882f9a26b472fc02701fe925d
yakov matan <yakovmatan@gmail.com>
2025-05-27 12:30:02 +0300

    create abstract class firearm

---
+++ Firearm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal abstract class Firearm
+    {
+        public string Name { get; }
+        public string Maker { get; }
+        public string Ammocount { get; protected set; }
+    }
+}
