4e2e13bfec272bdad89d0ce38fb3f2d3e136db80
yakov matan <yakovmatan@gmail.com>
2025-05-27 11:22:46 +0300

    fix commando class

--- Commando.cs
+++ Commando.cs
@@ -8,7 +8,40 @@ namespace Commandos
 {
     internal class Commando
     {
-        public string Name { get; } = "uhdf";
+        public string Name { get; }
+        public string CodeName { get; }
+
+        public Object Tool { get; }
+        public int CountLife { get; private set; } = 100;
+        public string status { get; private set; } = "walk";
+
+        public Commando(string Name, string CodeName, object tool)
+        {
+            this.Name = Name;
+            this.CodeName = CodeName;
+            Tool = tool;
+        }
+
+        public void walk()
+        {
+            Console.WriteLine("The soldier is walking.");
+            this.status = "walk";
+        }
+
+        public void Hide()
+        {
+            Console.WriteLine("The soldier is hiding.");
+            this.status = "hide";
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"The soldier {this.CodeName} attacking.");
+            this.status = "attack";
+        }
+
+
+
 
     }
 }
