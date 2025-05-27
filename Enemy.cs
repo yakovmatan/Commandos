5fe0bb3dc9f98b509434fbeca2b1c6bef886aaf5
yakov matan <yakovmatan@gmail.com>
2025-05-27 15:35:22 +0300

    create Enemy class

---
+++ Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class Enemy
+    {
+        public string Name { get; }
+        public int Life { get; private set; } = 100;
+        public bool IsLife { get; set; } = true;
+
+        public Enemy(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Hit()
+        {
+            this.Life -= 10;
+            IfDead();
+        }
+
+        public void IfDead()
+        {
+            if(this.Life <= 0)
+            {
+                this.IsLife = false;
+            }    
+        }
+
+        public void Shout()
+        {
+            Console.WriteLine("i'm enemy");
+        }
+    }
+}
